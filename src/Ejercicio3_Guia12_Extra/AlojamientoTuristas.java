2f2f1746a3ed411d82d05c742bd4e06908cb2fe4
Jose Luis Elisseche <joselise61@gmail.com>
2023-06-26 11:20:13 -0300

    Actualizar

--- src/Ejercicio3_Guia12_Extra/AlojamientoTuristas.java
+++ src/Ejercicio3_Guia12_Extra/AlojamientoTuristas.java
@@ -20,98 +20,102 @@ import Ejercicio3_Guia12_Extra_entidades.Residencia;
 
 public class AlojamientoTuristas {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Alojamiento> alojamientos = new ArrayList<>();
-        alojamientos.add(new HotelCuatroEstrellas("Hotel A", "Calle 123", "Ciudad A", "Gerente A",
-                100, 200, 5, "A", "Restaurante A", 40));
-        alojamientos.add(new HotelCincoEstrellas("Hotel B", "Calle 456", "Ciudad B", "Gerente B",
-                200, 400, 10, "B", "Restaurante B", 60, 5, 10, 3));
-        alojamientos.add(new Camping("Camping C", "Calle 789", "Ciudad C", "Gerente C",
-                true, 1000, 50, 10, true));
-        alojamientos.add(new Residencia("Residencia D", "Calle 987", "Ciudad D", "Gerente D",
-                false, 500, 30, true, false));
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Alojamiento> alojamientos = new ArrayList<>();
+            alojamientos.add(new HotelCuatroEstrellas("Hotel A", "Calle 123", "Ciudad A", "Gerente A",
+                    100, 200, 5, "A", "Restaurante A", 40));
+            alojamientos.add(new HotelCincoEstrellas("Hotel B", "Calle 456", "Ciudad B", "Gerente B",
+                    200, 400, 10, "B", "Restaurante B", 60, 5, 10, 3));
+            alojamientos.add(new Camping("Camping C", "Calle 789", "Ciudad C", "Gerente C",
+                    true, 1000, 50, 10, true));
+            alojamientos.add(new Residencia("Residencia D", "Calle 987", "Ciudad D", "Gerente D",
+                    false, 500, 30, true, false));
 
-        int opc;
-        do {
-            System.out.println("Seleccione una opcion: ");
-            System.out.println("1. Listar todos los alojamientos");
-            System.out.println("2. Listar hoteles del mas caro al mas barato");
-            System.out.println("3. Listar campings con restaurant");
-            System.out.println("4. Listar residencias con descuentos");
-            System.out.println("5. Salir !");
-            opc = sc.nextInt();
+            int opc;
+            do {
+                System.out.println("Seleccione una opcion: ");
+                System.out.println("1. Listar todos los alojamientos");
+                System.out.println("2. Listar hoteles del mas caro al mas barato");
+                System.out.println("3. Listar campings con restaurant");
+                System.out.println("4. Listar residencias con descuentos");
+                System.out.println("5. Salir !");
+                opc = sc.nextInt();
 
-            switch (opc) {
-                case 1:
-                // Consulta: todos los alojamientos
-                System.out.println("\nTodos los alojamientos:");
-                for (Alojamiento alojamiento : alojamientos) {
-                System.out.println("Nombre: " + alojamiento.getNombre());
-                System.out.println("Dirección: " + alojamiento.getDireccion());
-                System.out.println("Localidad: " + alojamiento.getLocalidad());
-                System.out.println("Gerente: " + alojamiento.getGerente());
-                System.out.println();
-                    } break;             
-                case 2:
-                //Consulta: Hoteles de más caro a más barato
-                ArrayList<Hotel> hoteles = new ArrayList<>();
-                for (Alojamiento alojamiento : alojamientos) {
-                if (alojamiento instanceof Hotel) {
-                hoteles.add((Hotel) alojamiento);
-                }
-                 }
-                System.out.println("Hoteles de más caro a más barato:");
-                Collections.sort(hoteles, new Comparator<Hotel>() {
-                @Override
-                public int compare(Hotel h1, Hotel h2) {
-                return Double.compare(h2.getCantidadHabitaciones(), h1.getCantidadHabitaciones());
-                }
-                });
-
-                for (Hotel hotel : hoteles) {
-                System.out.println("Nombre: " + hotel.getNombre());
-                System.out.println("Dirección: " + hotel.getDireccion());
-                System.out.println("Localidad: " + hotel.getLocalidad());
-                System.out.println("Gerente: " + hotel.getGerente());
-                System.out.println("Precio habitaciones: " + hotel.getCantidadHabitaciones());
-                System.out.println();
-                }break;
-                case 3:
-                // Consulta: todos los campings con restaurante
-                System.out.println("Campings con restaurante:");
-                for (Alojamiento alojamiento : alojamientos) {
-                if (alojamiento instanceof Camping) {
-                Camping camping = (Camping) alojamiento;
-                if (camping.tieneRestaurante()) {
-                    System.out.println("Nombre: " + camping.getNombre());
-                    System.out.println("Dirección: " + camping.getDireccion());
-                    System.out.println("Localidad: " + camping.getLocalidad());
-                    System.out.println("Gerente: " + camping.getGerente());
+                switch (opc) {
+                    case 1:
+                    // Consulta: todos los alojamientos
+                    System.out.println("\nTodos los alojamientos:");
+                    for (Alojamiento alojamiento : alojamientos) {
+                    System.out.println("Nombre: " + alojamiento.getNombre());
+                    System.out.println("Dirección: " + alojamiento.getDireccion());
+                    System.out.println("Localidad: " + alojamiento.getLocalidad());
+                    System.out.println("Gerente: " + alojamiento.getGerente());
                     System.out.println();
-                }
-                }
-                }break;
-                case 4:
-                // Consulta: todas las residencias que tienen descuento
-                System.out.println("Residencias con descuento:");
-                for (Alojamiento alojamiento : alojamientos) {
-                if (alojamiento instanceof Residencia) {
-                Residencia residencia = (Residencia) alojamiento;
-                if (residencia.tieneDescuentoGremios()) {
-                    System.out.println("Nombre: " + residencia.getNombre());
-                    System.out.println("Dirección: " + residencia.getDireccion());
-                    System.out.println("Localidad: " + residencia.getLocalidad());
-                    System.out.println("Gerente: " + residencia.getGerente());
+                        } break;             
+                    case 2:
+                    //Consulta: Hoteles de más caro a más barato
+                    ArrayList<Hotel> hoteles = new ArrayList<>();
+                    for (Alojamiento alojamiento : alojamientos) {
+                    if (alojamiento instanceof Hotel) {
+                    hoteles.add((Hotel) alojamiento);
+                    }
+                     }
+                    System.out.println("Hoteles de más caro a más barato:");
+                    Collections.sort(hoteles, new Comparator<Hotel>() {
+                    @Override
+                    public int compare(Hotel h1, Hotel h2) {
+                    return Double.compare(h2.getCantidadHabitaciones(), h1.getCantidadHabitaciones());
+                    }
+                    });
+
+                    for (Hotel hotel : hoteles) {
+                    System.out.println("Nombre: " + hotel.getNombre());
+                    System.out.println("Dirección: " + hotel.getDireccion());
+                    System.out.println("Localidad: " + hotel.getLocalidad());
+                    System.out.println("Gerente: " + hotel.getGerente());
+                    System.out.println("Precio habitaciones: " + hotel.getCantidadHabitaciones());
                     System.out.println();
-                }
-                }
-                }break;
-                default:
-                System.out.println("Ingrese opcion correcta");;
-                break;   
-            }            
-        } while (opc != 5);
+                    }break;
+                    case 3:
+                    // Consulta: todos los campings con restaurante
+                    System.out.println("Campings con restaurante:");
+                    for (Alojamiento alojamiento : alojamientos) {
+                    if (alojamiento instanceof Camping) {
+                    Camping camping = (Camping) alojamiento;
+                    if (camping.tieneRestaurante()) {
+                        System.out.println("Nombre: " + camping.getNombre());
+                        System.out.println("Dirección: " + camping.getDireccion());
+                        System.out.println("Localidad: " + camping.getLocalidad());
+                        System.out.println("Gerente: " + camping.getGerente());
+                        System.out.println();
+                    }
+                    }
+                    }break;
+                    case 4:
+                    // Consulta: todas las residencias que tienen descuento
+                    System.out.println("Residencias con descuento:");
+                    for (Alojamiento alojamiento : alojamientos) {
+                    if (alojamiento instanceof Residencia) {
+                    Residencia residencia = (Residencia) alojamiento;
+                    if (residencia.tieneDescuentoGremios()) {
+                        System.out.println("Nombre: " + residencia.getNombre());
+                        System.out.println("Dirección: " + residencia.getDireccion());
+                        System.out.println("Localidad: " + residencia.getLocalidad());
+                        System.out.println("Gerente: " + residencia.getGerente());
+                        System.out.println();
+                    }
+                    }
+                    }break;
+                    default:
+                    System.out.println("Ingrese opcion correcta");;
+                    break;   
+                }            
+            } while (opc != 5);
+        }
 
         
     }
